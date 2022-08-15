8732e7a2496888d2e37e1bdacf26347f30b146c2
txleadfoot00 <106290919+txleadfoot00@users.noreply.github.com>
2022-08-15 02:06:52 -0700

    commit tree 3f8291318876ba073b5ede6d7081f3a82d3fe4e8

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return [drivers[0], drivers[1]];
+    }
+
+const returnLastTwoDrivers = function(drivers) {
+    return [drivers[drivers.length-2], drivers[drivers.length-1]];
+}
+const selectingDrivers = [returnFirstTwoDrivers(drivers), returnLastTwoDrivers(drivers)];
